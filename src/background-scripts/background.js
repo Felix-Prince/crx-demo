148190c3b70b3ebc153c0b3befe5b7318e983cd0
PengfeiZhu <zhupengfei@caicloud.io>
2020-07-17 14:48:26 +0800

    background 向 popup 通信

--- src/background-scripts/background.js
+++ src/background-scripts/background.js
@@ -1,3 +1,8 @@
-function sayHello(name){
-    console.log("hello",name)
+// function sayHello(name){
+//     console.log("hello",name)
+// }
+
+const views = chrome.extension.getViews({ type: "popup" });
+if(views.length > 0){
+    console.log(views[0].document.body.innerText)
 }
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,6 @@
-chrome.runtime.getBackgroundPage(function callback(backgroundPage){
-    backgroundPage.sayHello("felix")
-})
+// chrome.runtime.getBackgroundPage(function callback(backgroundPage){
+//     backgroundPage.sayHello("felix")
+// })
 
 // const page = chrome.extension.getBackgroundPage()
 // page.sayHello("felix")
