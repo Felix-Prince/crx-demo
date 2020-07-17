f44c5143c2b5b7e42efb58c414ec23cc74fa566d
PengfeiZhu <zhupengfei@caicloud.io>
2020-07-17 17:20:46 +0800

    content 向 background 通信

--- src/background-scripts/background.js
+++ src/background-scripts/background.js
@@ -2,7 +2,13 @@
 //     console.log("hello",name)
 // }
 
-const views = chrome.extension.getViews({ type: "popup" });
-if(views.length > 0){
-    console.log(views[0].document.body.innerText)
-}
+// const views = chrome.extension.getViews({ type: "popup" });
+// if(views.length > 0){
+//     console.log(views[0].document.body.innerText)
+// }
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	console.log("收到来自content-script的消息：");
+	console.log(request, sender, sendResponse);
+	sendResponse("我是后台，我已收到你的消息：" + JSON.stringify(request));
+});
--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,10 +1,16 @@
-chrome.runtime.onMessage.addListener(function (
-	request,
-	sender,
-	sendResponse
-) {
-	if (request.cmd == "test") {
-		alert(request.value);
+// chrome.runtime.onMessage.addListener(function (
+// 	request,
+// 	sender,
+// 	sendResponse
+// ) {
+// 	if (request.cmd == "test") {
+// 		alert(request.value);
+// 	}
+// 	sendResponse("我收到了popup的信息");
+// });
+chrome.runtime.sendMessage(
+	{ greeting: "你好，我是content-script呀，我主动发消息给后台！" },
+	function (response) {
+		console.log("收到来自后台的回复：" + response);
 	}
-	sendResponse("我收到了popup的信息");
-});
+);
