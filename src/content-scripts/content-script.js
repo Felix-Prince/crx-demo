ccea0e0259c11ab566482f2c8b26a5fd3d66ebb6
PengfeiZhu <zhupengfei@caicloud.io>
2020-07-17 17:59:24 +0800

    inject script 向 content 通信

--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -8,9 +8,35 @@
 // 	}
 // 	sendResponse("我收到了popup的信息");
 // });
-chrome.runtime.sendMessage(
-	{ greeting: "你好，我是content-script呀，我主动发消息给popup！" },
-	function (response) {
-		console.log("收到来自pop的回复：" + response);
-	}
+// chrome.runtime.sendMessage(
+// 	{ greeting: "你好，我是content-script呀，我主动发消息给popup！" },
+// 	function (response) {
+// 		console.log("收到来自pop的回复：" + response);
+// 	}
+// );
+
+function injectCustomJs(jsPath)
+{
+	jsPath = jsPath || 'content-scripts/inject-script.js';
+	var temp = document.createElement('script');
+	temp.setAttribute('type', 'text/javascript');
+	// 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+	temp.src = chrome.extension.getURL(jsPath);
+	temp.onload = function()
+	{
+		// 放在页面不好看，执行完后移除掉
+		this.parentNode.removeChild(this);
+    };
+	(document.head || document.documentElement).appendChild(temp);
+
+}
+
+injectCustomJs()
+
+window.addEventListener(
+	"message",
+	function (e) {
+		console.log("content-script",e.data);
+	},
+	false
 );
